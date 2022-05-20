7778b0c2ac1acbc11985dd2bfa8e2eed12d54d9c
nygula <569878931@qq.com>
2022-05-20 09:17:30 +0800

    添加项目文件。

---
+++ BNR.Core.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BNR.Core;
+
+string rule = "{CN:出库}{S:WORLD}{D:yyyyMMddHHmmssfff}{N:{S:ORDER}/00000000000}"; //用户手填
+
+for (int i = 0; i < 1012; i++)
+{
+    string value = BNRFactory.Default.Create(rule);
+    Console.WriteLine(value);
+
+}
+Console.Read();
---
+++ BNR.Core/ChineseSpellCode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.International.Converters.PinYinConverter;
+
+namespace BNR.Core
+{
+    /// <summary>
+    /// {CN:广州}
+    /// </summary>
+    [ParameterType("CN")]
+    public class ChineseSpellCodeParameter : IParameterHandler
+    {
+        public void Execute(StringBuilder sb, string value)
+        {
+
+            foreach (char item in value)
+            {
+                var sp = FirstPinYin(item.ToString());
+             //   sb.Append(PinYin.GetFirst(item.ToString()));
+                sb.Append(sp);
+            }
+
+        }
+        /// <summary>
+        /// 汉字转首拼
+        /// </summary>
+        /// <param name="chinese">汉字</param>
+        /// <returns></returns>
+        public  string FirstPinYin(string chinese)
+        {
+            string retValue = string.Empty;
+
+            foreach (char chr in chinese)
+            {
+                try
+                {
+                    if (ChineseChar.IsValidChar(chr))
+                    {
+                        ChineseChar chineseChar = new ChineseChar(chr);
+                        string t = chineseChar.Pinyins[0].ToString();
+                        retValue += t.Substring(0, 1);
+                    }
+                    else
+                    {
+                        retValue += chr.ToString();
+                    }
+                }
+                catch (Exception e)
+                {
+                    return "Z";
+                }
+            }
+
+            return retValue;
+        }
+
+
+        public BNRFactory Factory
+        {
+            get;
+            set;
+        }
+    }
+}
